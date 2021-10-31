a65ff96b5d307f9b0628391a64a70a38f19b1030
giovannidmilana@gmail.com <giovannidmilana@gmail.com>
2021-10-31 17:39:30 -0400

    first commit

--- rewards/views.py
+++ rewards/views.py
@@ -28,13 +28,14 @@ def index(request):
         context = {'user' : request.user, 'customer_form' : customer_form, 'employee' : employee}
         return render(request, 'rewards/index1.html', context)
     else:
+        '''
         employee = get_object_or_404(Employee, user_id = request.user.id)
         customer_form = CustomerForm()
         context = {'user' : request.user, 'customer_form' : customer_form, 'employee' : employee}
         return render(request, 'rewards/index1.html', context)
+        '''
         
-        
-        #return HttpResponseRedirect(reverse('rewards:login'))
+        return HttpResponseRedirect(reverse('rewards:login'))
         
 class login_view(LoginView):
     template_name = 'rewards/login1.html'
